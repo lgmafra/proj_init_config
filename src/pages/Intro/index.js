64486b746723b7f40f6d38472de2263028d19e4a
Luiz Gustavo Sampaio Mafra de Santana <lgmafra@gmail.com>
2019-06-13 10:35:50 -0300

    fix app removing react-navigation-hooks lib

--- src/pages/Intro/index.js
+++ src/pages/Intro/index.js
@@ -2,18 +2,16 @@ import React from 'react';
 import {
   View, Text, TouchableOpacity, Image, ImageBackground, StatusBar,
 } from 'react-native';
-import { useNavigation } from 'react-navigation-hooks';
+import PropTypes from 'prop-types';
 
 import styles from './styles';
 
 import image from '~/images/image.png';
 import logo from '~/images/logo.png';
 
-export default function Intro() {
-  const { navigate } = useNavigation();
-
+export default function Intro({ navigation }) {
   return (
-    <ImageBackground style={styles.container} resizeMode="cover" source={image}>
+    <ImageBackground style={styles.container} resizeMode="cover" source={image} testID="background">
       <StatusBar barStyle="light-content" />
       <View style={styles.textInfo}>
         <Text style={styles.info}>Investimento</Text>
@@ -22,7 +20,11 @@ export default function Intro() {
       </View>
       <Text style={styles.infoLogin}>Faça seu login ou crie sua conta</Text>
       <View style={styles.buttonsPlace}>
-        <TouchableOpacity style={styles.buttonLogin} onPress={() => navigate('Login')}>
+        <TouchableOpacity
+          testID="buttonLogin"
+          style={styles.buttonLogin}
+          onPress={() => navigation.navigate('Login')}
+        >
           <Text style={styles.buttonText}>Login</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.buttonNew} onPress={() => {}}>
@@ -35,3 +37,9 @@ export default function Intro() {
     </ImageBackground>
   );
 }
+
+Intro.propTypes = PropTypes.shape({
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func,
+  }),
+}).isRequired;
